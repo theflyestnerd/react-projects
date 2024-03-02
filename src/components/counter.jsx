d9ab0950db032291ebf268aa768fb665699b24c3
theflyestnerd <kolaolari@gmail.com>
2024-03-02 23:12:20 +0100

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        count : 0,
+        tags: []
+    };
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    }
+
+    render() {
+        return(
+            <div>
+                {this.state.tags.length === 0 && "Please create a new tag!"}
+                {this.renderTags()}
+                </div>
+        ) ;
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+    }
+
+    formatCount() {
+        const {count} = this.state;
+        return count === 0 ? "Zero" : count;
+    }
+}
+ 
+export default Counter;
